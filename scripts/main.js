d1af527ddc42761774a0d8c85c5735fba53a1814
James Jushchuk <jushchuk@ibm.com>
2019-10-31 15:57:11 -0400

    display tables

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+// functions for loading in csv and running callback function upon success //
 function xhrSuccess() { 
     this.callback.apply(this, this.arguments); 
 }
@@ -7,7 +8,7 @@ function xhrError() {
 }
 
 function loadFile(url, callback /*, opt_arg1, opt_arg2, ... */) {
-    var xhr = new XMLHttpRequest();
+    let xhr = new XMLHttpRequest();
     xhr.callback = callback;
     xhr.arguments = Array.prototype.slice.call(arguments, 2);
     xhr.onload = xhrSuccess;
@@ -16,19 +17,69 @@ function loadFile(url, callback /*, opt_arg1, opt_arg2, ... */) {
     xhr.send(null);
 }
 
+// functions for manipulating CSV //
 function parseCSV(CSVstring) {
     arr = CSVstring.split('\n')
     console.log(arr);
-    var jsonObj = [];
-    var headers = arr[0].split(',');
+    let jsonObj = [];
+    let headers = arr[0].split(',');
     console.log(headers)
-    for(var i = 1; i < arr.length; i++) {
-        var data = arr[i].split(',');
-        var obj = {};
-        for(var j = 0; j < data.length; j++) {
+    for(let i = 1; i < arr.length; i++) {
+        let data = arr[i].split(',');
+        let obj = {};
+        for(let j = 0; j < data.length; j++) {
             obj[headers[j].trim()] = data[j].trim();
         }
         jsonObj.push(obj);
     }
     return jsonObj;
+}
+
+function parseDataToGames(data) {
+    let games = [];
+    let game = [];
+    let currentGame = data[0]['Game'];
+    for (let i=0; i<data.length; i++) {
+        if (currentGame == data[i]['Game']) {
+            game.push(data[i]);
+        } else {
+            games.push(game);
+            currentGame = data[i]['Game'];
+            game = [data[i]];
+        }
+    }
+    return games;
+}
+
+function createTable(gameData, captionText, tableName) {
+    let ignoreProperties = ['Players','Player','Winner','Game'];
+
+    //create table and caption
+    let table = document.createElement('table');
+    let caption = table.createCaption();
+    caption.textContent = captionText;
+    
+    //determine row keys
+    let keys = ['Player'];
+    for (let property in gameData[0]) {
+        if(!ignoreProperties.includes(property)) {
+            keys.push(property);
+        }
+    }
+
+    //for each row key, populate the row
+    for (let k in keys) {
+        let row = table.insertRow(-1);
+        let cell = row.insertCell(-1);
+        cell.textContent = keys[k];
+        for(let i=0; i<gameData.length; i++) {
+            cell = row.insertCell(-1);
+            cell.textContent = gameData[i][keys[k]];
+        }
+    }
+
+    //handle
+    let thead = table.createTHead();
+    let tbody = table.createTBody();
+    return table
 }
--- scripts/tables.js
+++ scripts/tables.js
@@ -1,12 +1,40 @@
 function populateTables(){
-    data = parseCSV(this.responseText);
-    console.log(data);
+    let data = parseCSV(this.responseText);
+    let games = parseDataToGames(data);
+    
+    let tables_container = document.getElementById('tables_container')
+    
+    for (let i=0; i<games.length; i++) {
+        let gameNumber = games[i][0]['Game'];
+        let gamePlayers = [];
+        for (let j=0; j<games[i].length; j++) {
+            //determine who are the players for a particular game
+            let player = games[i][j]['Player'];
+            gamePlayers.push([player]);
+            
+            //this is to help later know who are all possible players
+            if (!allPlayers.includes(player)) {
+                allPlayers.push(player);
+            }
+        }
+
+        //create the actual html table
+        let table = createTable(games[i],'Game '+gameNumber);
+        
+        //push this table with some info to potentially use later
+        tables.push({'Game':gameNumber, 'Players':gamePlayers, 'Table':table});
+
+        //add table to page... potentially do this another place
+        tables_container.appendChild(table);
+    }
 }
 
+var tables = [];
+var allPlayers = [];
 const dataURL = 'https://raw.githubusercontent.com/jushchuk/agricola/master/data/gric-refined.csv';
-var data = loadFile(dataURL, populateTables);
-
+loadFile(dataURL, populateTables);
 
 setTimeout(function(){
-    console.log(data);
+    console.log(tables);
+    console.log(allPlayers);
 }, 500);
