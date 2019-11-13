df604e67e7855929254b0d71e8cb3ee0602bf251
James Jushchuk <jushchuk@ibm.com>
2019-11-12 19:46:31 -0500

    added filter by threshold

--- scripts/tables.js
+++ scripts/tables.js
@@ -21,12 +21,12 @@ function createTable(gameData, captionText) {
         let row = table.insertRow(-1);
         let cell = row.insertCell(-1);
         cell.textContent = keys[k];
-        for(let i=0; i<5; i++) {
+        for (let i = 0; i < 5; i++) {
             cell = row.insertCell(-1);
-            if(i<gameData.length) {
+            if (i < gameData.length) {
                 cell.textContent = gameData[i][keys[k]];
 
-                if(gameData[i]['Winner'] == 'True'){
+                if (gameData[i]['Winner'] == 'True') {
                     cell.className = 'winner';
                 }
             }
@@ -40,43 +40,128 @@ function createTable(gameData, captionText) {
 }
 
 function submitFilter() {
-    let playerSelect = document.getElementById('player_select');
-    let validPlayers = playerSelect.selectedOptions;
-    filterTables(validPlayers);
+    let validPlayers = document.getElementById('player_select').selectedOptions;
+    let exactPlayersMatch = document.getElementById('exact_players_match_checkbox').checked;
+    
+    let thresholdType = document.getElementById('threshold_type_select').selectedOptions[0].value;
+    let thresholdComparison = document.getElementById('threshold_comparison_select').selectedOptions[0].value;
+    let thresholdValue = parseInt(document.getElementById('threshold_value_textbox').value);
+
+    let threshold = {'type': thresholdType, 'value': thresholdValue, 'comparison': thresholdComparison};
+
+
+    filterTables(validPlayers, exactPlayersMatch, threshold);
 }
 
-function filterTables(validPlayers) {
-    //hide games that do not contain validPlayers
-    visibleCount = 0;
-    if (validPlayers.length > 0) {
-        console.log(tables[0].Players);
-        console.log(validPlayers[0].value);
-        for (let i = 0; i < tables.length; i++) {
-            let display = 'block';
+function filterTables(validPlayers, exactPlayersMatch, threshold) {
+    //hide games that do match filter
+
+    let visibleCount = 0;
+
+    for (let i = 0; i < tables.length; i++) {
+        
+        let display = 'block';
+
+        //filter for players
+        if (validPlayers.length > 0) {
             for (let j = 0; j < validPlayers.length; j++) {
                 if (!tables[i].Players.includes(validPlayers[j].value)) {
                     display = 'none';
                 }
+                if (exactPlayersMatch && tables[i].Players.length != validPlayers.length) {
+                    display = 'none';
+                }
             }
-            g = document.getElementById('game'+tables[i].Game);
-            if(display == 'block') { visibleCount++; }
-            g.style.display = display;
         }
+        
+        //filter for threshold
+        
+        let gameStats = produceGameStats(tables[i].GameData);
+        
+        if (!meetsThreshold(gameStats, threshold)) {
+            display = 'none';
+        }
+
+
+        let gameTable = document.getElementById('game'+tables[i].Game);
+        if (display == 'block') { visibleCount++; }
+        gameTable.style.display = display;
+    }
+
+    if (visibleCount == tables.length) {
+        document.getElementById('table_count_span').textContent = 'Showing all ' + visibleCount + ' tables';
+    } else {
+        document.getElementById('table_count_span').textContent = 'Showing ' + visibleCount + ' tables';
     }
     
-    console.log(visibleCount);
+}
+
+function produceGameStats(gameData) {
+    let playerCount = gameData.length;
+    let sum = 0;
+    let max = 0;
+    let min = Infinity;
+    for (let j = 0; j < playerCount; j++) {
+        let score = parseInt(gameData[j]['Total']);
+        sum += score;
+        if (max < score) {
+            max = score;
+        }
+        if (min > score) {
+            min = score;
+        }
+    }
+
+    let average = sum / playerCount;
+    return {'average': average, 'max': max, 'margin': max-min};
+}
+
+function meetsThreshold(gameStats, threshold) {
+    if (isNaN(threshold.value)) {
+        return true;
+    }
+    let gameValue;
+    switch (threshold.type) {
+        case 'Winner\'s':
+            gameValue = gameStats.max;
+            break;
+        case 'Average':
+            gameValue = gameStats.average;
+            break;
+        case 'Margin':
+            gameValue = gameStats.margin;
+            break;
+    }
+
+    let result;
+    switch (threshold.comparison) {
+        case '>':
+            result = (gameValue > threshold.value);
+            break;
+        case '<':
+            result = (gameValue < threshold.value);
+            break;
+        case '=':
+            result = (Math.round(gameValue) == threshold.value);
+            break;
+    }
+
+    return result;
 }
 
 function resetFilter() {
-    console.log('Reseting Filter');
-    
     //reset each filter option
     let playerOptions = document.getElementById("player_select").options;
-
+    
     for (let i = 0; i < playerOptions.length; i++) {
       playerOptions[i].selected = false;
     }
 
+    //reset checkbox
+    document.getElementById('exact_players_match_checkbox').checked = false;
+
+    //reset threshold
+    document.getElementById('threshold_value_textbox').value = '';
 
     //now show all games
     for (let i = 1; i < tables.length + 1; i++) {
@@ -84,12 +169,14 @@ function resetFilter() {
         t.style.display = 'block';
     }
 
-    //reset visibleCount to all
-    visibleCount = tables.length;
+    //reset table count 
+    document.getElementById('table_count_span').textContent = 'Showing all ' + tables.length + ' tables';
 }
 
 function populateFilterTools() {
     let filterDiv = document.getElementById('table_filter');
+    
+    //player filter
 
     let playerSelect = document.createElement('select');
     playerSelect.id = 'player_select';
@@ -101,6 +188,42 @@ function populateFilterTools() {
         playerSelect.add(option);
     }
 
+    let exactPlayersMatchCheckbox = document.createElement('input');
+    exactPlayersMatchCheckbox.id = 'exact_players_match_checkbox';
+    exactPlayersMatchCheckbox.type = 'checkbox';
+    let exactPlayersMatchCheckboxLabel = document.createElement('label');
+    exactPlayersMatchCheckboxLabel.textContent = 'Exact Players Match';
+    exactPlayersMatchCheckboxLabel.htmlFor = 'exact_players_match_checkbox'
+    
+    // threshold filter
+    let thresholdTypeSelect = document.createElement('select');
+    thresholdTypeSelect.id = 'threshold_type_select';
+
+    for (let i=0; i<thresholdTypes.length; i++) {
+        let option = document.createElement('option');
+        option.value = thresholdTypes[i];
+        option.text = thresholdTypes[i];
+        thresholdTypeSelect.add(option);
+    }
+
+    let thresholdComparisonSelect = document.createElement('select');
+    thresholdComparisonSelect.id = 'threshold_comparison_select';
+
+    for (let i=0; i<thresholdComparisons.length; i++) {
+        let option = document.createElement('option');
+        option.value = thresholdComparisons[i];
+        option.text = thresholdComparisons[i];
+        thresholdComparisonSelect.add(option);
+    }
+
+    let thresholdValueTextbox = document.createElement('input');
+    thresholdValueTextbox.id = 'threshold_value_textbox';
+    thresholdValueTextbox.type = 'textbox';
+    thresholdValueTextbox.placeholder = 'Score';
+    thresholdValueTextbox.pattern = '[0-9]+';
+
+    //buttons
+
     let filterSubmit = document.createElement('button');
     filterSubmit.textContent = 'Filter';
     filterSubmit.onclick = submitFilter;
@@ -108,10 +231,24 @@ function populateFilterTools() {
     let filterReset = document.createElement('button');
     filterReset.textContent = 'Reset';
     filterReset.onclick = resetFilter;
+
+    //count span
+
+    let filterTableCount = document.createElement('span');
+    filterTableCount.id = 'table_count_span';
+    filterTableCount.textContent = 'Showing all ' + tables.length + ' tables';
     
+    //append it all
+
     filterDiv.append(playerSelect);
+    filterDiv.append(exactPlayersMatchCheckboxLabel);
+    filterDiv.append(exactPlayersMatchCheckbox);
+    filterDiv.append(thresholdTypeSelect);
+    filterDiv.append(thresholdComparisonSelect);
+    filterDiv.append(thresholdValueTextbox);
     filterDiv.append(filterSubmit);
     filterDiv.append(filterReset);
+    filterDiv.append(filterTableCount);
 
 }
 
@@ -146,7 +283,7 @@ function populateTables() {
         let table = createTable(games[i],'Game '+gameNumber);
         
         //push this table with some info to potentially use later
-        tables.push({'Game':gameNumber, 'Players':gamePlayers, 'Table':table});
+        tables.push({'Game':gameNumber, 'Players':gamePlayers, 'Table':table, 'GameData':games[i]});
 
         //add table to page... potentially do this another place
         let div = wrapWithDiv(table, 'game'+gameNumber, 'table_div');
@@ -168,11 +305,12 @@ function populateTables() {
 var tables = [];
 var allPlayers = [];
 var sortedPlayerCounts = [];
-var visibleCount = 0;
+
+const thresholdTypes = ['Winner\'s', 'Average', 'Margin'];
+const thresholdComparisons = ['>', '<', '='];
 const dataURL = 'https://raw.githubusercontent.com/jushchuk/agricola/master/data/gric-refined.csv';
 loadFile(dataURL, populateTables);
 
 setTimeout(function(){
-    console.log('loaded (hopefully)');
-    console.log(sortedPlayerCounts);
+    console.log('data loaded (hopefully)');
 }, 500);
